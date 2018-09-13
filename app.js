aa654de2e133a5fc972687dcb83ccbbee26899db
Adam Asshidiq <a.asshidiq@gmail.com>
2018-09-13 22:31:53 +0700

    fix basic routes

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ var app = express();
 
 // View Engine
 app.set('views', path.join(__dirname, 'views'));
-app.engine(bodyParser.urlencoded({ extended: false }));
+app.engine('handlebars', exphbs({defaultLayout: 'layout'}));
+app.set('view engine', 'handlebars');
+
+// Bodyparser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 // Set Static Folder
